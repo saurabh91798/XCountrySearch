844d52d13d32e3fa00e13ca54ba73d260b7eff78
K.Saurabh Rao <saurabh91798@gmail.com>
2024-08-05 12:05:49 +0530

    copied from xcountry and added search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        const data = response.json();
+        return data;
+      })
+      .then((data) => {
+        setCountries(data);
+        setFilteredCountries(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredCountries(filtered);
+  }, [countries, searchText]);
+
+  const handleSearch = (event) => {
+    setSearchText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="searchbar">
+          <input
+            type="text"
+            value={searchText}
+            onChange={handleSearch}
+            placeholder="Search for countries..."
+          />
+        </div>
+        <div className="countries">
+          {filteredCountries.map((country, index) => (
+            <div key={index} className="countryCard">
+              <img src={country.flags.png} alt={country.name.common} />
+              <p>{country.name.common}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
